b589ed3b38065ca682880378d72c3c2fccf7d772
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-17 01:07:09 +0530

    route

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomePage from "./Components/HomePage";
+import Navbar from "./Components/Navbar";
+
+import { Routes2 } from "./Components/Routes";
+function App() {
+  return (
+    <div className="App">
+      
+      <Routes2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Routes.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import ProductsPage from "./ProductsPage";
+import ProductsDetailsPage from "./ProductsDetailsPage";
+export const Routes2 = () => {
+  return (
+    <>
+      <Navbar />
+      {/* Add Routes here */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductsDetailsPage />} />
+        
+      </Routes>
+    </>
+  );
+};
